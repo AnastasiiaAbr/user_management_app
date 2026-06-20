5f6e3a7c637a6079f9bc9d41595cafb24ce1c97f
AnastasiiaAbr <zalogova.an@yandex.ru>
2026-06-20 19:00:35 +0500

    new route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
         <Route path='/login'
           element={<LoginPage />}
         />
